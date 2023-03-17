862a4cc1a9454b8e4ea1bffc9c40b122b285526f
Sanket <sanketpatil7467@gmail.com>
2023-03-17 10:32:35 +0530

    Fourth Commit

--- app/src/main/java/com/example/faceheal/UserRegistration.java
+++ app/src/main/java/com/example/faceheal/UserRegistration.java
@@ -1,14 +1,55 @@
 package com.example.faceheal;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class UserRegistration extends AppCompatActivity {
+    private final int GALLERY_REQ_CODE = 1000;
+    private Button upload_image;
+    private Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_registration);
+
+        upload_image = findViewById(R.id.upload_image);
+        submit = findViewById(R.id.submit);
+
+        upload_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iGallery = new Intent(Intent.ACTION_PICK);
+                iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(iGallery,GALLERY_REQ_CODE);
+            }
+        });
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK){
+            if (requestCode == GALLERY_REQ_CODE){
+                Toast.makeText(this, "Image Selected", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else{
+            Toast.makeText(this, "Failed to select image", Toast.LENGTH_SHORT).show();
+        }
     }
 }
