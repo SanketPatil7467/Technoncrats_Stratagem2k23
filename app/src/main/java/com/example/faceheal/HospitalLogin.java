9415a377c732bff2c33cebad3cd60e69afae1321
Sanket <sanketpatil7467@gmail.com>
2023-03-17 23:34:27 +0530

    Ninth Commit

--- app/src/main/java/com/example/faceheal/HospitalLogin.java
+++ app/src/main/java/com/example/faceheal/HospitalLogin.java
@@ -1,5 +1,6 @@
 package com.example.faceheal;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,7 +14,16 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class HospitalLogin extends AppCompatActivity {
+
+    DatabaseReference reference;
+    FirebaseDatabase db;
     ImageView imgCamera;
     EditText phone_number;
     Button take_image;
@@ -21,10 +31,16 @@ public class HospitalLogin extends AppCompatActivity {
     TextView match_status;
     Button end_session;
 
+    boolean sta;
+
+    int counter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hospital_login);
+
+        counter =1;
+        sta = false;
 
         imgCamera = findViewById(R.id.imageView);
         phone_number = findViewById(R.id.phone_number);
@@ -32,6 +48,56 @@ public class HospitalLogin extends AppCompatActivity {
         verify = findViewById(R.id.verify);
         match_status = findViewById(R.id.match_status);
         end_session = findViewById(R.id.end_session);
+
+
+
+        verify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String field = phone_number.getText().toString();
+                db =FirebaseDatabase.getInstance("https://faceheal-default-rtdb.firebaseio.com/");
+                reference = db.getReference("Users");
+
+                reference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                        for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                            String key = dataSnapshot.child("Phone").getValue().toString();
+                            String cand_name = dataSnapshot.child("Name").getValue().toString();
+                            String cand_email = dataSnapshot.child("Email").getValue().toString();
+                            System.out.println(dataSnapshot.getValue());
+                            System.out.println(key);
+                            System.out.println("Hello");
+                            System.out.println(field);
+
+
+
+                            if (key.equals(field)){
+                                String str = "Counter Number : "+String.valueOf(counter)+"\nName: "+cand_name+"\nEmail: "+cand_email;
+                                match_status.setText(str);
+                                sta = true;
+                                counter = counter+1;
+                                break;
+
+
+                            }
+
+                        }
+                        if (sta==false){
+                            match_status.setText("Not Matching Candidate");
+                        }
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+        });
 
 
         take_image.setOnClickListener(new View.OnClickListener() {
