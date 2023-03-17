6870ba3759bd4b6ef0e4126a76694321ebf378a3
Sanket <sanketpatil7467@gmail.com>
2023-03-17 07:56:15 +0530

    Second Commit

---
+++ app/src/main/java/com/example/faceheal/HospitalLogin.java
@@ -0,0 +1,14 @@
+package com.example.faceheal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HospitalLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospital_login);
+    }
+}
--- app/src/main/java/com/example/faceheal/MainActivity.java
+++ app/src/main/java/com/example/faceheal/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.faceheal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button hospital_login;
+    private Button user_registration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hospital_login =findViewById(R.id.hospital_login);
+        user_registration = findViewById(R.id.user_registration);
+
+        hospital_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, HospitalLogin.class));
+            }
+        });
+
+        user_registration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, UserRegistration.class));
+            }
+        });
     }
 }
